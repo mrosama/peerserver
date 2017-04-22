208b257ade065331003e73cfb4f9e17e29a64117
Osama Salama <osama_eg@outlook.com>
2017-04-22 20:49:29 +0200

    add index js

---
+++ index.js
@@ -0,0 +1,10 @@
+var PeerServer = require('peer').PeerServer;
+var server = PeerServer({port: 9000, path: '/peerjs'});
+
+
+/*
+
+var PeerServer = require('peer').PeerServer;
+var server = PeerServer({port: 9000, path: '/myapp', proxied: true});
+
+ */
